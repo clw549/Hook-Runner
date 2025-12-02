b71a4c2fb46aee46215434ea782f11053688ba91
Ciaran Wham <clw549@nau.edu>
2025-12-02 10:05:01 -0700

    starting work on grappling hook and other helper functions

--- extension/GrapplingHook.cpp
+++ extension/GrapplingHook.cpp
@@ -1,5 +1,6 @@
 #include <godot_cpp/core/class_db.hpp>
 #include "GrapplingHook.h"
+#include <godot_cpp/core/binder_common.hpp>  
 
 using namespace godot;
 
@@ -7,10 +8,32 @@ void GrapplingHook::_bind_methods() {
   ClassDB::bind_method(D_METHOD("ShootHook", "direction"), &GrapplingHook::ShootHook);
   ClassDB::bind_method(D_METHOD("RetractHook"), &GrapplingHook::RetractHook);
   ClassDB::bind_method(D_METHOD("SetMaxHookLength", "length"), &GrapplingHook::SetMaxHookLength);
+  ClassDB::bind_method(D_METHOD("_ready"), &GrapplingHook::_ready);
+
+  ClassDB::bind_method(D_METHOD("GetGrapplingHook"), &GrapplingHook::GetGrapplingHook);
+  ClassDB::bind_method(D_METHOD("SetGrapplingHook", "hook"), &GrapplingHook::SetGrapplingHook);
+  ClassDB::bind_method(D_METHOD("SetSpeed", "newSpeed"), &GrapplingHook::SetSpeed);
+  ClassDB::bind_method(D_METHOD("GetSpeed"), &GrapplingHook::GetSpeed);
+  ClassDB::bind_method(D_METHOD("SetPlayer", "newPlayer"), &GrapplingHook::SetPlayer);
+  ClassDB::bind_method(D_METHOD("GetPlayer"), &GrapplingHook::GetPlayer);
+
+  ClassDB::add_property("GrapplingHook", PropertyInfo(Variant::OBJECT, "grapplingHook", PROPERTY_HINT_NODE_TYPE, "Node2D"), "SetGrapplingHook", "GetGrapplingHook");
+
+  ClassDB::add_property("GrapplingHook", PropertyInfo(Variant::FLOAT, "speed", PROPERTY_HINT_NODE_TYPE, "float"), "SetSpeed", "GetSpeed");
+
+  ClassDB::add_property("GrapplingHook", PropertyInfo(Variant::OBJECT, "_player", PROPERTY_HINT_NODE_TYPE, "Node2D"), "SetPlayer", "GetPlayer");
+
+  ClassDB::bind_method(D_METHOD("OnHookHitBody", "body"), &GrapplingHook::OnHookHitBody);
+
+}
+
+void GrapplingHook::_ready() {
+  collider = get_node<CollisionShape2D>("CollisionShape2D");
+  collider->connect("body_entered", Callable(this, "OnHookHitBody"));
 }
 
 GrapplingHook::GrapplingHook() {
-  _graplingHook = nullptr;
+  speed = 50;
 
 }
 
@@ -18,12 +41,31 @@ GrapplingHook::~GrapplingHook() {
 
 }
 
-Node2D* GrapplingHook::ShootHook(Vector2 direction) {
+void GrapplingHook::_physics_process(double delta) {
+    velocity = _shoot_direction * speed;
+
+    // move and get collision
+    Ref<KinematicCollision2D> collision = move_and_collide(velocity * delta);
+
+    if (collision.is_valid()) {
+        OnHookHitBody(collision.ptr()); 
+    }
+}
+
+void GrapplingHook::ShootHook(Vector2 direction) {
+  _shoot_direction = direction.normalized();
+
   //todo: instantiate child pointing in the direction of direction
-  return NULL;
 }
 
 void GrapplingHook::RetractHook() {
+
+}
+
+void GrapplingHook::OnHookHitBody(KinematicCollision2D *collision) {
+  _shoot_direction = Vector2(0,0);
+  UtilityFunctions::print("Hook touched body");
+
 
 }
 
@@ -33,4 +75,26 @@ void GrapplingHook::AimGuide(bool show) {
 
 void GrapplingHook::SetMaxHookLength(float length) {
 
+}
+
+void GrapplingHook::SetGrapplingHook(Node2D *hook) {
+  grapplingHook = hook;
+}
+
+Node2D *GrapplingHook::GetGrapplingHook(){
+  return grapplingHook;
+}
+
+void GrapplingHook::SetSpeed(float newSpeed) {
+  speed = newSpeed;
+}
+float GrapplingHook::GetSpeed() {
+  return speed;
+}
+
+void GrapplingHook::SetPlayer(Node2D *newPlayer) {
+  _player = newPlayer
+}
+Node2D *GrapplingHook::GetPlayer() {
+  return _player;
 }
--- extension/GrapplingHook.h
+++ extension/GrapplingHook.h
@@ -1,31 +1,55 @@
 #ifndef GRAPPLINGHOOK
 #define GRAPPLINGHOOK
 
+#include <godot_cpp/classes/character_body2d.hpp>
 #include <godot_cpp/classes/node2d.hpp>
 #include <cstdlib>
 #include <godot_cpp/godot.hpp>
+#include <godot_cpp/classes/collision_shape2d.hpp>
 
 namespace godot {
-  class GrapplingHook : public Node2D {
-    GDCLASS(GrapplingHook, Node2D)
+  class GrapplingHook : public CharacterBody2D {
+    GDCLASS(GrapplingHook, CharacterBody2D)
 
     private:
       Vector2 _hookPosition;
-      float _maxLength;
-      float _currentLength;
+      float _maxLength = 200;
+      float _currentLength = 50;
       bool _showGuide;
-      Node2D* _graplingHook;
-    
+      bool _hooked = false;
+      void OnHookHitBody(KinematicCollision2D *collision);
+      Vector2 _shoot_direction = Vector2(1, 0);
+      Node2D *_player;
+
+      
+
     protected:
       static void _bind_methods();
 
     public:
+      Node2D* grapplingHook;
+      float speed;
+      CollisionShape2D *collider;
+      Vector2 velocity = Vector2(1,0);
+
+      void _physics_process(double delta);
+
+      void _ready();
       GrapplingHook();
       ~GrapplingHook();
-      Node2D* ShootHook(Vector2 direction);
+      void ShootHook(Vector2 direction);
       void RetractHook();
       void AimGuide(bool show);
       void SetMaxHookLength(float length);
+
+      void SetGrapplingHook(Node2D *hook);
+      Node2D *GetGrapplingHook();
+
+      void SetSpeed(float speed);
+      float GetSpeed();
+
+      Node2D *GetPlayer();
+      void SetPlayer(Node2D newPlayer);
   };
 }
 
